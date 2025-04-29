4a56a593fb8518c676c154c4bce218b5afb5604e
Andres Mauricio Salamanca Arias <andres.salamanca@mirai.com.co>
2025-04-29 16:15:52 -0500

    probando

--- back/app/tests/test_main.py
+++ back/app/tests/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_create_user():
     response = client.post("/users/", params={"name": "Juan", "email": "juan@example.com"})
     assert response.status_code == 200
-    assert response.json()["name"] == "Juan"
+    assert response.json()["name"] == "Juan" 
 
 def test_read_users():
     response = client.get("/users/")
