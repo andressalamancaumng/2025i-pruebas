e780a64fbd09fdef6152063d8e2a6dc4dc9682f5
LorenaCU20 <est.jiseth.cardenas@unimilitar.edu.co>
2025-05-03 21:56:25 -0500

    Taller 28 de abril

--- back/app/crud.py
+++ back/app/crud.py
@@ -1,12 +1,33 @@
 from sqlalchemy.orm import Session
 from app.models import User
 
-def create_user(db: Session, name: str, email: str):
-    db_user = User(name=name, email=email)
+def create_user(db: Session, name: str, email: str, documento: int):
+    db_user = User(name=name, email=email, documento=int)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+def delete_user_by_id(db:Session, user_id:int):
+    user=db.query(User).filter(User.id==user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False
+    
 def get_users(db: Session):
     return db.query(User).all()
+
+def get_users_by_id(db:Session, users_id:integer):
+    return db.query(models.User).filter(models.User.id==user_id).first()
+
+def get_users_by_name(db:Session, name:str):
+    return db.query(models.User).filter(models.User.name==name).first()    
+
+def get_users_by_email(db:Session, name:str):
+    return db.query(models.User).filter(models.User.email==email).first()
+
+def get_user_by_documento(db:Session, documento:int):
+    return db.query(models.User).filter(models.User.documento==documento).first()
+
--- back/app/main.py
+++ back/app/main.py
@@ -14,9 +14,26 @@ def get_db():
         db.close()
 
 @app.post("/users/")
-def create_user(name: str, email: str, db: Session = Depends(get_db)):
-    return crud.create_user(db, name, email)
+def create_user(name: str, email: str, documento: int, db: Session = Depends(get_db)):
+    return crud.create_users(db, name, email)
 
 @app.get("/users/")
 def read_users(db: Session = Depends(get_db)):
     return crud.get_users(db)
+    
+@app.get("/users/{user_id}")
+def read_users_by_id(user_id:integer, db:Session=Depends(get_db)):
+    return crud.get_users_by_id(db,user_id)
+
+@app.get("/users/name/{name}")
+def read_users_by_name(name:str, db:Session=Depends(get_db)):
+    return crud.get_users_by_name(db,name)
+
+@app.get("/users/email/{email}")
+def read_users_by_email(email:str, db:Session=Depends(get_db)):
+    return crud.get_users_by_email(db,email)
+    
+@app.get("/users/documento/{documento}")
+def read_users_by_documento(documento:int, db:Session=Depends(get_db)):
+    return crud.get_users_by_documento(db,documento)
+
