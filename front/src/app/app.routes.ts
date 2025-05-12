020edfd57f886e97ec11e1f8f9cfa248330b4c03
Simón Paipa <simonpaipab@gmail.com>
2025-05-12 16:20:21 -0500

    1.1 front con back

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -15,7 +15,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'listado-usuarios',
+    redirectTo: 'usuarios',
     pathMatch: 'full',
   },
   
--- front/src/app/pages/listado-usuarios/listado-usuarios.page.ts
+++ front/src/app/pages/listado-usuarios/listado-usuarios.page.ts
@@ -14,6 +14,7 @@ export class ListadoUsuariosPage {
   nombre = '';
   email = '';
   documento = '';
+  usuarios = [];
 
   constructor(private userService: UserService) {}
 
