50ce2a3b5db4c676b899c7eb44d489b985627cf3
Isabella Diaz <est.isabella.diazp@unimilitar.edu.co>
2025-05-13 12:55:43 -0500

    Cambio pruebas

--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   private apiUrl = 'http://localhost:8000';
 
   constructor(private http: HttpClient) {}
-
+  
   getUsers(): Observable<any> {
     return this.http.get(`${this.apiUrl}/users/`);
   }
