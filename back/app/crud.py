82726fec12f271adf2b59fecdd19f7c2ec416cd7
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2025-05-13 21:58:01 -0500

    campo documento y delete

--- back/app/crud.py
+++ back/app/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from app.models import User
 
 def create_user(db: Session, name: str, email: str, documento:str):
-    db_user = User(name=name, email=email,Ndocumento=documento)
+    db_user = User(name=name, email=email,documento=documento)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -10,3 +10,23 @@ def create_user(db: Session, name: str, email: str, documento:str):
 
 def get_users(db: Session):
     return db.query(User).all()
+
+def get_userID(db:Session,userID:int):
+    return db.query(User).filter(User.ID==userID).first()
+
+def get_name(db:Session,name:int):
+    return db.query(User).filter(User.name==name).first()
+
+def get_email(db:Session,email:int):
+    return db.query(User).filter(User.email==email).first()
+
+def get_documento(db:Session,documento:int):
+    return db.query(User).filter(User.documento==documento).first()
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return user
+    return None
