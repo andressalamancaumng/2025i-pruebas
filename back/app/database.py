c0a7192208d6b7cb3c239cc5cba348292e790fee
Sara Martinez <est.sara.vmartinez@unimilitar.edu.co>
2025-05-14 08:09:26 -0500

    inicializacion_database

--- back/app/database.py
+++ back/app/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 import os
+
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:123456@localhost/testdb")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+base = declarative_base()
+def init_db():
+    from app import models  # Importa los modelos
+    base.metadata.drop_all(bind=engine)  # Borra tablas de las bases de datos
+    base.metadata.create_all(bind=engine)  # Crea las tablas de acuerdo a los modelos
+
+
