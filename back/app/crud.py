f147316e4b32d40bf1c44e0b6021401f09952b06
JuanUrrego146 <juandavidurregofonseca677@gmail.com>
2025-05-17 18:01:13 -0500

    Correcciones de funcionamiento BACKEND

--- back/app/crud.py
+++ back/app/crud.py
@@ -12,17 +12,19 @@ def create_pelicula(db: Session, nombre: str, anio: int, director: str):
 def get_peliculas(db: Session):
     return db.query(Pelicula).all()
 
-def get_pelicula_by_id(db: Session, id: int):
-    return db.query(Pelicula).filter(Pelicula.id == id).first()
 def delete_pelicula(db: Session, id: int):
     pelicula = db.query(Pelicula).filter(Pelicula.id == id).first()
-    if pelicula:
-        db.delete(pelicula)
-        db.commit()
+    if not pelicula:
+        raise Exception("Pelicula no encontrada")
     
+    
+    db.delete(pelicula)
+    db.commit()
+    
+    try:
         db.execute(text("SET @count = 0;"))
         db.execute(text("UPDATE peliculas SET id = (@count := @count + 1);"))
         db.execute(text("ALTER TABLE peliculas AUTO_INCREMENT = 1;"))
         db.commit()
-    else:
-        raise Exception("Pelicula no encontrada")
+    except Exception as e:
+        raise Exception(f"Error al ajustar IDs: {str(e)}")
--- back/app/models.py
+++ back/app/models.py
@@ -8,8 +8,6 @@ class Pelicula(Base):
     __tablename__ = "peliculas"  
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(50), index=True)
-    email = Column(String(50), unique=True, index=True)
     nombre = Column(String(100), index=True)
     anio = Column(Integer, index=True)
     director = Column(String(100), index=True) 
