442e71b5ddf1d87bfaeecf846112f4e5f6cdf2ca
Lyannn10 <est.dylan.guzman@unimilitar.edu.co>
2025-05-17 11:57:12 -0500

    Entrega final

--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from './user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
