1f2a67fc535ef46c6ba56ca6dab033e17f4e6eed
lyn <lyqtjs2@gmail.com>
2025-05-16 23:00:14 -0500

    pruebas3

--- back/app/schemas.py
+++ back/app/schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 class PeliculaBase(BaseModel):
     nombre: str = Field(..., example="Inception")
@@ -18,4 +18,4 @@ class Pelicula(PeliculaBase):
     id: int
 
     class Config:
-        orm_mode = True
+      model_config = ConfigDict(from_attributes=True)
