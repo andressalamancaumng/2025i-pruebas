8f0890b0ba7edcbab674e87180cdccc3a5724e2b
lyn <lyqtjs2@gmail.com>
2025-05-17 15:26:52 -0500

    home.page.ts

--- front/src/app/home/home.page.ts
+++ front/src/app/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular'; // ✅ Importar IonicModule aquí
+import { IonicModule } from '@ionic/angular'; 
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true, // ✅ Muy importante
+  standalone: true, 
   imports: [CommonModule, IonicModule],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
