230bddd44a779437311017ea890a51f1f012ae5e
lyn <lyqtjs2@gmail.com>
2025-05-16 23:04:55 -0500

    pruebas5

--- back/app/tests/tests/test_main.py
+++ back/app/tests/tests/test_main.py
@@ -7,12 +7,13 @@ def test_create_pelicula():
     response = client.post(
         "/peliculas/",
         json={
-            "nombre": "Inception",
+            "name_movie": "Inception",
             "anio": 2010,
             "director": "Christopher Nolan"
         }
     )
     assert response.status_code == 200
+
     data = response.json()
     assert data["nombre"] == "Inception"
     assert data["anio"] == 2010
