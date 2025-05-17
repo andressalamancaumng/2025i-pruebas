4fd2a827320af180555d711973130672271537f6
Jeimmy Cortes <est.jeimmy.cortes@unimilitar.edu.co>
2025-05-16 22:07:20 -0500

    front

--- front/src/app/pages/listado-carros/listado-carros.page.ts
+++ front/src/app/pages/listado-carros/listado-carros.page.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { CarsService } from '../../services/carro.service';
 
 @Component({
   selector: 'app-listado-carros',
   templateUrl: './listado-carros.page.html',
   styleUrls: ['./listado-carros.page.scss'],
+  standalone: true,
+  imports: [IonicModule, NgFor, AsyncPipe, RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ListadoCarrosPage implements OnInit {
   carros: any[] = [];
@@ -20,21 +26,26 @@ export class ListadoCarrosPage implements OnInit {
   }
 
   cargarCarros() {
-    this.carroService.getCarros().subscribe(data => {
-      this.carros = data;
+    this.carroService.getCarros().subscribe({
+      next: (data) => {
+        console.log('Carros recibidos:', data);
+        this.carros = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener carros', err);
+      }
     });
   }
 
   crearCarro() {
+    if (!this.nuevoModelo || !this.nuevaMarca || !this.nuevaSerie) return;
+
     this.carroService.createCars(this.nuevoModelo, this.nuevaMarca, this.nuevaSerie)
       .subscribe(() => {
         this.nuevoModelo = 0;
         this.nuevaMarca = '';
         this.nuevaSerie = '';
-        this.cargarCarros(); // recarga la lista para mostrar el nuevo carro
+        this.cargarCarros();  // recarga la lista
       });
   }
 }
-
-
-
