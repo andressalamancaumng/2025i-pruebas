17dd3886a4f76cd71694d0d3c0ae49c3ab9d0d98
Stefany Gelvez <est.yulieth.gelvez@unimilitar.edu.co>
2025-05-17 15:04:53 -0500

    prueba 4

--- front/src/app/pages/crear-carro/crear-carro.page.spec.ts
+++ front/src/app/pages/crear-carro/crear-carro.page.spec.ts
@@ -1,11 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CrearCarroPage } from './crear-carro.page';
+import { CarroService } from '../services/carro.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('CrearCarroPage', () => {
   let component: CrearCarroPage;
   let fixture: ComponentFixture<CrearCarroPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CrearCarroPage ],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      providers: [ CarroService ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CrearCarroPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- front/src/app/pages/listar-carros/listar-carros.page.spec.ts
+++ front/src/app/pages/listar-carros/listar-carros.page.spec.ts
@@ -1,11 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListarCarrosPage } from './listar-carros.page';
+import { CarroService } from '../services/carro.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ListarCarrosPage', () => {
   let component: ListarCarrosPage;
   let fixture: ComponentFixture<ListarCarrosPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListarCarrosPage ],
+      imports: [ HttpClientTestingModule ],
+      providers: [ CarroService ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ListarCarrosPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
