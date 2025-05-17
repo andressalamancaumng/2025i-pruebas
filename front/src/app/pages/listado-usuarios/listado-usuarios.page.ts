1e5185fe07f34493b498158c8dedfd41a024ff20
Samuelfdezurrea <est.samueld.fernan1@unimilitar.edu.co>
2025-05-16 22:42:52 -0500

    Listado de usuarios

--- front/src/app/pages/listado-usuarios/listado-usuarios.page.ts
+++ front/src/app/pages/listado-usuarios/listado-usuarios.page.ts
@@ -1,29 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { AsyncPipe, NgFor } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { UserService } from '../../services/user.service';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-
+import { UserService, Usuario } from '../services/user.service';
 
 @Component({
   selector: 'app-listado-usuarios',
   templateUrl: './listado-usuarios.page.html',
-  styleUrls: ['./listado-usuarios.page.scss'],
-  standalone: true,
-  imports: [IonicModule, NgFor, AsyncPipe, RouterModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ListadoUsuariosPage implements OnInit {
-  usuarios: any[] = [];
-  constructor(private userService: UserService) {
-    this.userService.getUsers().subscribe((data) =>{
-      this.usuarios=data;
-    });
-   }
+  usuarios: Usuario[] = [];
+  nombre = '';
+  correo = '';
+  documento = '';
+  mensaje = '';
+
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.cargarUsuarios();
   }
 
+  cargarUsuarios() {
+    this.userService.getUsuarios().subscribe((data) => {
+      this.usuarios = data;
+    });
+  }
+
+  buscar() {
+    this.mensaje = '';
+    if (this.nombre) {
+      this.userService.buscarPorNombre(this.nombre).subscribe({
+        next: (res) => (this.usuarios = res),
+        error: () => (this.mensaje = 'No se encontró por nombre.'),
+      });
+    } else if (this.correo) {
+      this.userService.buscarPorCorreo(this.correo).subscribe({
+        next: (res) => (this.usuarios = [res]),
+        error: () => (this.mensaje = 'No se encontró por correo.'),
+      });
+    } else if (this.documento) {
+      this.userService.buscarPorDocumento(this.documento).subscribe({
+        next: (res) => (this.usuarios = [res]),
+        error: () => (this.mensaje = 'No se encontró por documento.'),
+      });
+    } else {
+      this.mensaje = 'Ingresa al menos un criterio de búsqueda.';
+    }
+  }
+
+  eliminar(id: number) {
+    if (confirm('¿Seguro que quieres eliminar este usuario?')) {
+      this.userService.eliminarUsuario(id).subscribe({
+        next: () => {
+          this.mensaje = 'Usuario eliminado correctamente.';
+          this.cargarUsuarios();
+        },
+        error: () => {
+          this.mensaje = 'Error al eliminar usuario.';
+        },
+      });
+    }
+  }
+
+  limpiarCampos() {
+    this.nombre = '';
+    this.correo = '';
+    this.documento = '';
+    this.mensaje = '';
+    this.cargarUsuarios();
+  }
 }
+
--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -1,22 +1,49 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+export interface Usuario {
+  id: number;
+  nombre: string;
+  correo: string;
+  documento: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8000';
+  private apiUrl = 'http://localhost:8000/usuarios'; // Ajusta si es diferente
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/users/`);
+  // Obtener todos los usuarios
+  getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.apiUrl}/`);
   }
 
-  createUser(name: string, email: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/users/`, null, {
-      params: { name, email },
-    });
+  // Crear usuario
+  crearUsuario(usuario: Omit<Usuario, 'id'>): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.apiUrl}/`, usuario);
+  }
+
+  // Eliminar por ID
+  eliminarUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  // Buscar por nombre
+  buscarPorNombre(nombre: string): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.apiUrl}/nombre/${nombre}`);
+  }
+
+  // Buscar por correo
+  buscarPorCorreo(correo: string): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.apiUrl}/correo/${correo}`);
+  }
+
+  // Buscar por documento
+  buscarPorDocumento(documento: string): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.apiUrl}/documento/${documento}`);
   }
 }
