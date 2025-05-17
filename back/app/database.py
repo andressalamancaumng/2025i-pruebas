b51d4eb7b9de3c29aa6f092cd096bf25c3876ceb
lyn <lyqtjs2@gmail.com>
2025-05-16 22:04:35 -0500

    database

--- back/app/database.py
+++ back/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:123456Qlocalhost/testdb")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:123456Qlocalhost3306/testdb")
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
