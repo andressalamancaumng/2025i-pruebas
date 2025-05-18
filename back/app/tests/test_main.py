10b2b8f1281f34ff2ee8caad96054f36f3deb971
Juan Barbosa <est.juan.cbarbosa@unimilitar.edu.co>
2025-05-17 21:32:28 -0500

    test2

--- back/app/tests/test_main.py
+++ back/app/tests/test_main.py
@@ -3,12 +3,12 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_create_user():
-    response = client.post("/users/", params={"name": "Juan", "email": "juan@example.com"})
+def test_create_cars():
+    response = client.post("/cars/", params={"modelo": "2010", "marca": "Ford","serie": "Explorer"})
     assert response.status_code == 200
-    assert response.json()["name"] == "Juan" 
+    assert response.json()["modelo"] == "2010" 
 
-def test_read_users():
-    response = client.get("/users/")
+def test_read_cars():
+    response = client.get("/cars/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
